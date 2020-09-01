d1d4e5b325b9d81a0edf37e275bce91b7d1ab97e
aidanclarkescott <clarkescotta@gmail.com>
2020-09-01 12:53:31 -0700

    Added whole block game to version control

---
+++ 3DBlockGame/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour {
+
+    public Transform playerPos;
+    public Vector3 cameraOffset;
+
+    // Update is called once per frame
+    void Update() {
+        transform.position = playerPos.position + cameraOffset;
+    }
+}
---
+++ 3DBlockGame/Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+    bool gameHasEnded = false;
+    public float restartDelay = 1f;
+    public GameObject completeLevelUI;
+    public PlayerMovement movement;
+
+    public void CompleteLevel() {
+        completeLevelUI.SetActive(true);
+        movement.enabled = false;
+    }
+
+    public void EndGame() {
+
+        if (!gameHasEnded) {
+            gameHasEnded = true;
+            Debug.Log("Game Over");
+            Invoke("Restart", restartDelay);
+        }
+
+    }
+
+    private void Restart() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
